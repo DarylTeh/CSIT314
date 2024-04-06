90ccd34c92fe9ace01bc9442e4806e3edac99ac5
Daryl Teh <d1358814@ncs.dev.net>
2024-04-06 22:46:20 +0800

    add files

--- DATABASE FILE/realestatephp.sql
+++ DATABASE FILE/realestatephp.sql
@@ -1,17 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2024 at 04:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
 -- Database: `realestatephp`
+--
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `about`
+--
 
 CREATE TABLE `about` (
   `id` int(10) NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` longtext NOT NULL,
   `image` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
 
 INSERT INTO `about` (`id`, `title`, `content`, `image`) VALUES
 (10, 'About Us', 'This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project. This is a demo about us page for this project.', 'condos-pool.png');
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
 
 CREATE TABLE `admin` (
   `aid` int(10) NOT NULL,
@@ -20,18 +54,30 @@ CREATE TABLE `admin` (
   `apass` varchar(50) NOT NULL,
   `adob` date NOT NULL,
   `aphone` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
 
 INSERT INTO `admin` (`aid`, `auser`, `aemail`, `apass`, `adob`, `aphone`) VALUES
-(9, 'admin', 'admin@gmail.com', '6812f136d636e737248d365016f8cfd5139e387c', '2001-12-14', '999');
+(9, 'admin', 'admin@gmail.com', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', '2001-12-14', '999');
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
 
 CREATE TABLE `city` (
   `cid` int(50) NOT NULL,
   `cname` varchar(100) NOT NULL,
   `sid` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `city`
+--
 
 INSERT INTO `city` (`cid`, `cname`, `sid`) VALUES
 (9, 'City 1', 3),
@@ -42,21 +88,9 @@ INSERT INTO `city` (`cid`, `cname`, `sid`) VALUES
 
 -- --------------------------------------------------------
 
-CREATE TABLE `state` (
-  `sid` int(50) NOT NULL,
-  `sname` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `state` (`sid`, `sname`) VALUES
-(2, 'Singapore'),
-(3, 'Malaysia'),
-(4, 'China'),
-(7, 'Ukraine'),
-(9, 'Vietnam'),
-(10, 'Indonesia'),
-(15, 'Africa');
-
--- --------------------------------------------------------
+--
+-- Table structure for table `contact`
+--
 
 CREATE TABLE `contact` (
   `cid` int(50) NOT NULL,
@@ -65,22 +99,34 @@ CREATE TABLE `contact` (
   `phone` varchar(20) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `message` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
 
 INSERT INTO `contact` (`cid`, `name`, `email`, `phone`, `subject`, `message`) VALUES
 (7, 'csci314', 'csci314@gmail.com', '999', 'csci314', 'Kachow');
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `feedback`
+--
+
 CREATE TABLE `feedback` (
   `fid` int(50) NOT NULL,
   `uid` int(50) NOT NULL,
   `fdescription` varchar(300) NOT NULL,
   `status` int(1) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
 
 CREATE TABLE `property` (
   `pid` int(50) NOT NULL,
@@ -112,11 +158,39 @@ CREATE TABLE `property` (
   `topmapimage` varchar(300) NOT NULL,
   `groundmapimage` varchar(300) NOT NULL,
   `totalfloor` varchar(50) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
   `isFeatured` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `sid` int(50) NOT NULL,
+  `sname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`sid`, `sname`) VALUES
+(2, 'Singapore'),
+(3, 'Malaysia'),
+(4, 'China'),
+(7, 'Ukraine'),
+(9, 'Vietnam'),
+(10, 'Indonesia'),
+(15, 'Africa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
   `uid` int(50) NOT NULL,
@@ -126,64 +200,128 @@ CREATE TABLE `user` (
   `upass` varchar(50) NOT NULL,
   `utype` varchar(50) NOT NULL,
   `uimage` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `user` (`uid`, `uname`, `uemail`, `uphone`, `upass`, `utype`) VALUES
-(28, 'Daryl', 'dteh69@gmail.com', '91813930', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(29, 'Yu Xiang', 'yeosh235@gmail.com', '97594385', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(30, 'Elliot', 'eting.jh@gmail.com', '83799675', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(31, 'Amos', 'Dekiimasen@gmail.com', '81834867', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(32, 'Chen Kang', 'ckswordsman@gmail.com', '87534310', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(33, 'Raasheetha', 'raasheetha19@gmail.com', '91457847', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(34, 'Cherl', 'Cheryltoooh@gmail.com', '81262934', '6812f136d636e737248d365016f8cfd5139e387c', 'agent'),
-(35, 'User', 'user@gmail.com', '999', '6812f136d636e737248d365016f8cfd5139e387c', 'user');
+--
+-- Dumping data for table `user`
+--
 
--- --------------------------------------------------------
+INSERT INTO `user` (`uid`, `uname`, `uemail`, `uphone`, `upass`, `utype`, `uimage`) VALUES
+(28, 'Daryl', 'dteh69@gmail.com', '91813930', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(29, 'Yu Xiang', 'yeosh235@gmail.com', '97594385', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(30, 'Elliot', 'eting.jh@gmail.com', '83799675', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(31, 'Amos', 'Dekiimasen@gmail.com', '81834867', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(32, 'Chen Kang', 'ckswordsman@gmail.com', '87534310', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(33, 'Raasheetha', 'raasheetha19@gmail.com', '91457847', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(34, 'Cherl', 'Cheryltoooh@gmail.com', '81262934', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'agent', 'no.png'),
+(35, 'User', 'user@gmail.com', '999', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'user', 'no.png'),
+(36, 'Builder', 'builder@gmail.com', '999', '32ca9fc1a0f5b6330e3f4c8c1bbecde9bedb9573', 'builder', 'no.png');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `about`
+--
 ALTER TABLE `about`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`aid`);
 
+--
+-- Indexes for table `city`
+--
 ALTER TABLE `city`
   ADD PRIMARY KEY (`cid`);
 
+--
+-- Indexes for table `contact`
+--
 ALTER TABLE `contact`
   ADD PRIMARY KEY (`cid`);
 
+--
+-- Indexes for table `feedback`
+--
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`fid`);
 
+--
+-- Indexes for table `property`
+--
 ALTER TABLE `property`
   ADD PRIMARY KEY (`pid`);
 
+--
+-- Indexes for table `state`
+--
 ALTER TABLE `state`
   ADD PRIMARY KEY (`sid`);
 
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`uid`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `about`
+--
 ALTER TABLE `about`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
+--
+-- AUTO_INCREMENT for table `admin`
+--
 ALTER TABLE `admin`
   MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
+--
+-- AUTO_INCREMENT for table `city`
+--
 ALTER TABLE `city`
   MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
+--
+-- AUTO_INCREMENT for table `contact`
+--
 ALTER TABLE `contact`
   MODIFY `cid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
+--
+-- AUTO_INCREMENT for table `feedback`
+--
 ALTER TABLE `feedback`
   MODIFY `fid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT for table `property`
+--
 ALTER TABLE `property`
   MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
+--
+-- AUTO_INCREMENT for table `state`
+--
 ALTER TABLE `state`
   MODIFY `sid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
+--
+-- AUTO_INCREMENT for table `user`
+--
 ALTER TABLE `user`
-  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
